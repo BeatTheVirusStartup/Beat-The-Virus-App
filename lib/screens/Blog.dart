a4a9fa79ed0e6f4ec6ace961fb0972d827c2d6e9
RohanUttamVeer <rohanveer04@gmail.com>
2021-05-01 00:48:33 +0530

    blog page: new blog updated

--- lib/screens/Blog.dart
+++ lib/screens/Blog.dart
@@ -10,23 +10,26 @@ class _BlogState extends State<Blog> {
   String dateTime = DateFormat('MMM, dd, yyyy').format(DateTime.now());
 
   List<String> imgLS = [
+    'assets/blog/blog4.webp',
     'assets/blog/blog.jpeg',
     'assets/blog/blog1.webp',
     'assets/blog/blog2.webp',
-    'assets/blog/blog3.webp'
+    'assets/blog/blog3.webp',
   ];
   List<String> titleLS = [
+    'Meditation: A way to reduce your stress and make you healthy',
     'Naturopathy',
     'Food Supplements: A necessity for good health',
     'Herbs in Germany that you can use for immunity boosting',
-    'Acupressure'
+    'Acupressure',
   ];
 
   List<String> contentLS = [
-    'Naturopathy is an art and science of healthy living and a drugless system of healing based on well-founded philosophy. Naturopathy is ‘holistic’ in its own approach by taking human body as a whole, as it treats the root cause of the disease and not just symptoms.',
+    'Meditation is a practice where an individual uses a technique such as mindfulness or focusing the mind on a particular object, thought, or activity to train attention and awareness, and achieve a mentally clear and emotionally calm and stable state.',
+    'Naturopathy is an art and science of healthy living and a drugless system of healing based on well-founded philosophy. Naturopathy is ‘holistic’ in its own approach by taking human body as a w0hole, as it treats the root cause of the disease and not just symptoms.',
     'A natural antioxidant source, Oregano Oil contains phytochemicals which support the body’s natural resistance. Gaia Herbs supercritical CO2 extract of Oregano volatile oils contains phenols, including Carvacrol and Thymol.',
     'In scientific words “immune system is a network of biological processes that protects an organism from disease. It detects and responds to a wide variety of pathogens, from viruses to parasites.” ',
-    'Acupressure is an alternative medicine technique often used for treatment of body pain , disease, pain in the joints or bones, treatment for stress etc.. It is based on the concepts of life energies which flows through ‘Meridians’  (energy network of body) in the body .'
+    'Acupressure is an alternative medicine technique often used for treatment of body pain , disease, pain in the joints or bones, treatment for stress etc.. It is based on the concepts of life energies which flows through ‘Meridians’  (energy network of body) in the body .',
   ];
 
   @override
